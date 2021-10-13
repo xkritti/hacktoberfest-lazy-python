a968b0043ac9757576593ef1672ba12a67b691e3
xkritti <krittamet0404@gmail.com>
2021-10-13 21:19:24 +0700

    delay_sleep

---
+++ lib/delay_sleep.py
@@ -0,0 +1,5 @@
+import time
+
+def sleep (value:float):
+    print ('slepp for {} .. '.format(value))
+    return time.sleep(value)
---
+++ main.py
@@ -0,0 +1,5 @@
+from lib.delay_sleep import sleep
+
+print("Hello .. ")
+sleep(2)
+print("come back from sleep .. ")
